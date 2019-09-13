8641017f771b9294bb4d47a04440c3846df2e3b9
edoardoproietti1997 <proioedoardo97@gmail.com>
2019-09-13 16:00:31 +0200

    comkmit del cazzo parte 2

--- src/main/java/it/dstech/controller/ControllerOne.java
+++ src/main/java/it/dstech/controller/ControllerOne.java
@@ -8,30 +8,38 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import it.dstech.connessioneDb.ConnessioneDb;
 
 @Controller
 public class ControllerOne {
-	
+
 	private List<String> continent = new ArrayList<String>();
-	
-	//properties
+
+	// properties
 	@Value("${welcome.message}")
-    private String message;
-	
+	private String message;
+
 	@GetMapping("/")
 	public String main(Model model) throws ClassNotFoundException, SQLException {
-			
-	ConnessioneDb cd = new ConnessioneDb();		
+
+		ConnessioneDb cd = new ConnessioneDb();
 		continent = (cd.cercaContinenti());
-		
+
 		model.addAttribute("message", message);
-	model.addAttribute("continent",continent);	
-		
+		model.addAttribute("continent", continent);
+
 		return "Welcome";
 	}
-	
-	
-	
+
+	@PostMapping("/cercaStato")
+	public String cercaStato(@RequestParam(name = "continent", required = true, defaultValue = "") String continente,
+			Model model) throws ClassNotFoundException, SQLException {
+		ConnessioneDb cd = new ConnessioneDb();
+		ArrayList<String> lista = new ArrayList<String>();
+		lista.addAll(cd.cercaStati(continente));
+		return "visualizzaStati";
+	}
 }
