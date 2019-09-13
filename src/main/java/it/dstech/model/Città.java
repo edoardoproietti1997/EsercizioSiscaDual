8d256aca873409b6877c8b900a77fa9960c89cd9
edoardoproietti1997 <proioedoardo97@gmail.com>
2019-09-13 12:53:18 +0200

    terzo commit

---
+++ src/main/java/it/dstech/model/Città.java
@@ -0,0 +1,16 @@
+package it.dstech.model;
+
+public class Città {
+
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	} 
+	
+	
+}
